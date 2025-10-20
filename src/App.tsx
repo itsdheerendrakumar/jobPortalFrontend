aefbb71d1c672a6e33b6d3fb40b0abf8f12e7e05
dheerendra kumar <testrpa85@gmail.com>
2025-10-21 01:26:57 +0530

    feat: render pages based on roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Login } from "./components/Login";
 import { Signup } from "./components/Signup";
 import { superAdminRoutes } from "./routes/superAdmin";
 import { adminRoutes } from "./routes/admin";
-const user: "admin" | "superAdmin" = "superAdmin";
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={<Layout />}>
-        {user === "superAdmin" && superAdminRoutes.map(({path, Page}) => (
-          <Route key={path} path={path} element={<Page />} />
-        ))}
+import { Toaster } from "sonner";
+import { useEffect } from "react";
+import { useProfileStore } from "./store/profile";
 
-        {user === "admin" && adminRoutes.map(({path, Page}) => (
-          <Route key={path} path={path} element={<Page />} />
-        ))}
-      </Route>
-    </Routes>
+export default function App() {
+
+    const {role} = useProfileStore();
+    const navigate = useNavigate()
+    useEffect(() => {
+      if(!localStorage.getItem("accessToken"))
+        navigate("/login")
+    }, [])
+
+  return (
+    <>
+      <Toaster 
+        position="top-right"
+      />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Layout />}>
+          {role === "superAdmin" && superAdminRoutes.map(({path, Page}) => (
+            <Route key={path} path={path} element={<Page />} />
+          ))}
+
+          {role === "admin" && adminRoutes.map(({path, Page}) => (
+            <Route key={path} path={path} element={<Page />} />
+          ))}
+        </Route>
+      </Routes>
+    </>
   )
 }
