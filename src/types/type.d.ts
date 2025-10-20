346cf965719f7acd309e30889d9b22985d09f986
dheerendra kumar <testrpa85@gmail.com>
2025-10-21 01:20:20 +0530

    feat: create type of api response

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,9 +1,9 @@
-interface Login {
+interface ILogin {
     email: string
     password: string
 }
 
-interface Signup extends Login {
+interface Signup extends ILogin {
     name?: string
     phone?: string
     country?: string
@@ -17,3 +17,21 @@ interface IAdminUserDetails {
     created: string
     status: "active" | "inactive"
 }
+
+interface ApiResponse<T> {
+    data: T
+    message: string
+}
+
+interface LoginData  {
+    token: string
+}
+
+interface ProfileData {
+    name: string
+    role: string
+    imageUrl: string | null
+}
+
+type LoginResponse = ApiResponse<LoginData>
+type ProfileResponse = ApiResponse<ProfileData>
