1c7eef336b90e15688d5cce6aebd9db08e2e970d
dheerendra kumar <testrpa85@gmail.com>
2025-10-23 00:52:21 +0530

    feat: create admin creation functionality.

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,4 +1,4 @@
-import { loginSchema } from "@/formValidation/validation";
+import { signupSchema } from "@/formValidation/validation";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Input } from "./ui/input";
@@ -6,12 +6,24 @@ import { Label } from "./ui/label";
 import { useForm, Controller, type SubmitHandler } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { FormValidationError } from "./customComponents/common/formValidationError";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { countryData } from "@/constants/country";
+import { useState } from "react";
+import { Checkbox } from "./ui/checkbox";
+import { useProfileStore } from "@/store/profile";
+import { useMutation } from "@tanstack/react-query";
+import type { CustomError } from "@/types/error";
+import { createNewAdmin } from "@/service/apis";
+import { toast } from "sonner";
+import { ButtonLoading } from "./customComponents/common/ButtonLoader";
 interface SignupProps {
     isCreateAdmin?: boolean
 }
 export function Signup({ isCreateAdmin }: SignupProps) {
 
-    const { control, handleSubmit, formState: { errors }, } = useForm({
+    const {role} = useProfileStore();
+    const [deletePermission, setDeletePermission] = useState(true);
+    const { control, handleSubmit, reset , formState: { errors }, } = useForm({
         defaultValues: {
             name: "",
             country: "",
@@ -19,14 +31,24 @@ export function Signup({ isCreateAdmin }: SignupProps) {
             email: "",
             password: "",
         },
-        resolver: yupResolver(loginSchema),
+        resolver: yupResolver(signupSchema),
 
     });
 
-    const onSubmit: SubmitHandler<Signup> = async (data) => {
-        console.log(data)
+    const onSubmit: SubmitHandler<ISignup> = async (data) => {
+        const payload = role === "superAdmin" ? { ...data, deletePermission } : data;
+        if(role === "superAdmin") {
+            newAdminResponse.mutate(payload);
+        }
     }
-
+    const newAdminResponse = useMutation<NewAdminResponse, CustomError, ISignup>({
+        mutationFn: (payload) => createNewAdmin(payload),
+        onSuccess:(data) => {
+            toast.success(data?.message)
+            reset();
+        }
+    })
+    const loading = newAdminResponse.isPending;
     return (
         <div className="flex items-center justify-center h-[calc(100vh - 32px)]">
             <Card className="w-full max-w-sm">
@@ -75,17 +97,39 @@ export function Signup({ isCreateAdmin }: SignupProps) {
                                 />
                                 {errors?.password && <FormValidationError message={errors?.password?.message} />}
                             </div>
-                            <div className="grid gap-2">
+                            <div className="grid gap-2 [&>button]:w-full">
                                 <Label htmlFor="name">Country</Label>
                                 <Controller
                                     name="country"
                                     control={control}
-                                    render={({ field }) => <Input {...field} />}
+                                    render={({ field }) => 
+                                    <Select onValueChange={field.onChange} value={field.value}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Country" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {countryData.map((country) => (
+                                                <SelectItem key={country} value={country}>
+                                                    {country}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>}
                                 />
                                 {errors?.country && <FormValidationError message={errors?.country?.message} />}
+                                
                             </div>
-                            <Button type="submit" className="w-full cursor-pointer">
-                                {isCreateAdmin ? "Create" : "Signup"}
+                            {role === "superAdmin" &&
+                                <div className="flex justify-between items-center gap-2.5">
+                                    <Label>Delete Permission</Label>
+                                    <Checkbox 
+                                        checked={deletePermission} 
+                                        onCheckedChange={(checked) => setDeletePermission(!!checked)}
+                                    />
+                                </div>
+                            }
+                            <Button type="submit" className={`w-full cursor-pointer`} disabled={loading}>
+                                {loading ? <ButtonLoading /> : (isCreateAdmin ? "Create" : "Signup")}
                             </Button>
                         </div>
                     </form>
