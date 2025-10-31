c2dfb471077b1836b013935490fc02c212595350
dheerendra kumar <testrpa85@gmail.com>
2025-11-01 01:00:00 +0530

    uiBug: don't make padding for all components. It is not as of design standards.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,11 +3,11 @@ import { Sidebar } from "./customComponents/common/Sidebar";
 
 export function Layout() {
     return(
-        <div className="p-4 bg-background min-h-screen">
+        <div className="bg-background min-h-screen">
             <div className="w-50 fixed top-4 left-4 bottom-4 rounded-sm p-2 shadow-md">
                 <Sidebar />
             </div>
-            <div className="ml-52 bg-accent min-h-[calc(100vh-2rem)] rounded-sm p-2">
+            <div className="ml-52 bg-accent min-h-[calc(100vh-2rem)] rounded-sm">
                 <Outlet />
             </div>
         </div>
