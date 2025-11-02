bbff9a2b0f3c0ad8d6dfd1572f122a227e8f26cf
dheerendra kumar <testrpa85@gmail.com>
2025-11-02 19:57:17 +0530

    make common constant file for users.

---
+++ src/constants/user.ts
@@ -0,0 +1,32 @@
+export const adminHeaders = [
+    "Name",	
+    "Email",	
+    "Phone",	
+    "Country",	
+    "Created",	
+    "Status",	
+    "Actions",
+]
+export const reviewerHeaders = [
+    "Name",	
+    "Email",	
+    "Phone",	
+    "Country",	
+    "Created At",	
+    "Status",	
+]
+
+export const reviewerHeadersSuperAdmin = [
+    "Name",	
+    "Email",	
+    "Phone",	
+    "Country",	
+    "Created At",	
+    "Status",
+    "Promote To Admin",
+]
+
+export const adminEndPoints = [
+    {endPoint: "/", label: "Dashboard"},
+    {endPoint: "/jobs", label: "Jobs"},
+]
