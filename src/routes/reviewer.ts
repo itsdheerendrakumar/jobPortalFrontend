6cb617a895b49fa8a5959c83e691c0616fe11a33
dheerendra kumar <testrpa85@gmail.com>
2025-11-13 01:55:24 +0530

    feat: add job page for reviewer also.

--- src/routes/reviewer.ts
+++ src/routes/reviewer.ts
@@ -1,5 +1,7 @@
+import { JobPage } from "@/pages/common/JobPage";
 import { Home } from "@/pages/reviwers/Home";
 
 export const reviewerRoutes = [
-    {path: "/", Page: Home}
+    {path: "/", Page: Home},
+    {path: "/job/:jobId", Page: JobPage}
 ]
