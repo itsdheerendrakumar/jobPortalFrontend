aeeae3d4b298150bca35119be3ff30ce79b492b4
dheerendra kumar <testrpa85@gmail.com>
2025-11-24 02:37:54 +0530

    import backend from env

--- src/service/apiInstance.ts
+++ src/service/apiInstance.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api/v1';
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: BACKEND_URL,
   timeout: 10000,
 });
 
